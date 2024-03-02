3597a796ab775225f969101c98f1c852d06f59db
jkemboe <jkemboe@gmail.com>
2024-03-02 22:41:30 +0300

    Title Char length

--- database/models.go
+++ database/models.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Blog struct {
 	gorm.Model
-	Title     string `gorm:"not null; size:50" json:"title" `
+	Title     string `gorm:"not null;" json:"title" `
 	Image_Url string `gorm:"not null;" json:"image_url"`
 	Author    string `gorm:"not null; size:25" json:"author"`
 	Body      string `gorm:"not null;" json:"body"`
