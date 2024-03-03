d8fd5913e7b6e58769985c9d68e57aba5a02b2a1
jkemboe <jkemboe@gmail.com>
2024-03-03 19:29:33 +0300

    create Json Web Token function implementation

--- handler/utils.go
+++ handler/utils.go
@@ -2,9 +2,34 @@ package handler
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 )
+
+// createJWTToken function creates a JWT Token and provides the client for future request validations
+func createJWTToken(username string) (string, error) {
+	var (
+		key []byte
+		t   *jwt.Token
+	)
+
+	key = []byte("f1Secrets")           // load from .env
+	t = jwt.New(jwt.SigningMethodHS256) // create new token
+
+	claims := t.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	claims["authorized"] = true
+	claims["username"] = username
+
+	s, err := t.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+}
 
 // validationErrors returns found errors stored in a slice and true if errors are found empty slice and false otherwise
 func validationErrors(err error) ([]string, bool) {
@@ -24,6 +49,7 @@ func validationErrors(err error) ([]string, bool) {
 	return errs, true
 }
 
+// comparePasswords function confirm if the passwords the user provides when registering are equal
 func comparePasswords(str1, str2 string) bool {
 	if str1 == str2 {
 		return true
