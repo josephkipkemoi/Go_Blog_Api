2466756f1d46dc86aecfd1704e66dbfa59e14dc5
jkemboe <jkemboe@gmail.com>
2024-03-03 18:14:02 +0300

    User Model

--- database/models.go
+++ database/models.go
@@ -2,6 +2,15 @@ package database
 
 import "gorm.io/gorm"
 
+type User struct {
+	gorm.Model
+	FirstName  string `gorm:"not null;" json:"firstName"`
+	LastName   string `gorm:"not null;" json:"lastName"`
+	Email      string `gorm:"not null;" json:"email"`
+	Password   string `gorm:"not null;" json:"password"`
+	RememberMe bool   `json:"rememberMe"`
+}
+
 type Blog struct {
 	gorm.Model
 	Title     string `gorm:"not null;" json:"title" `
