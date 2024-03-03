8a94b9babceec637859bed95b2ba9d549dadb644
jkemboe <jkemboe@gmail.com>
2024-03-03 18:20:24 +0300

    Register user input values

--- handler/structs.go
+++ handler/structs.go
@@ -1,5 +1,14 @@
 package handler
 
+type RegisterUserInput struct {
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirmPassword"`
+	Email           string `json:"email"`
+	RememberMe      bool   `json:"rememberMe"`
+}
+
 type PatchBlog struct {
 	Title     string `json:"title"`
 	Author    string `json:"author"`
