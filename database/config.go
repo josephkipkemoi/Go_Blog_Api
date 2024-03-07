e817b2bf3d0b7f0e2317946882df92e221039100
jkemboe <jkemboe@gmail.com>
2024-03-07 23:06:01 +0300

    Config file

---
+++ database/config.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+const (
+	host     = "dpg-cnhmv9779t8c73er5n9g-a"
+	port     = "5432"
+	user     = "f1secretsdb_user"
+	password = "o9oLdKmDUsE8jfqR2xq8mnOzWEfJjkFW"
+	dbname   = "f1secretsdb"
+)
+
+// ConnectServer is responsible for setting up and launching the database
+func ConnectDatabase() {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s	password=%s	dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	Db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Database connection error")
+		log.Fatalf("Error Message: %s", err)
+	}
+
+	fmt.Println("********************************")
+	fmt.Println("Database connected succesfully")
+	fmt.Println("********************************")
+
+	Db.AutoMigrate(
+		&User{},
+		&Blog{},
+	)
+
+	DB = Db
+}
