a88d7e0d834883def8a8acd9d2e90ca781ab12a5
jkemboe <jkemboe@gmail.com>
2024-03-07 21:16:57 +0300

    Load port from .env

--- src/main.go
+++ src/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"f1-blog/database"
 	"f1-blog/server"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-const (
-	port string = ":5050"
-)
+// Load Environment Variables
+func init() {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatalf("error loading .env file: %s", err)
+	}
+}
 
 func main() {
+	var port string = os.Getenv("APP_SERVER_PORT")
+
 	database.ConnectDatabase() // connect to DB
 
 	r := server.ConnectServer() // connect to server
