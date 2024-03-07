6be3cec8c9622efe1ad19d991c694d46dd0bf162
jkemboe <jkemboe@gmail.com>
2024-03-07 22:05:17 +0300

    env config

--- src/main.go
+++ src/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"f1-blog/database"
 	"f1-blog/server"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // Load Environment Variables
-// func init() {
-
-// 	err := godotenv.Load("./.env")
-// 	if err != nil {
-// 		log.Fatalf("error loading .env file: %s", err)
-// 	}
-// }
+func init() {
+	env := os.Getenv("APP_ENV")
+	if env == "development" || env == "" {
+		err := godotenv.Load("./.env")
+		if err != nil {
+			log.Fatalf("error loading .env file: %s", err)
+		}
+	}
+}
 
 func main() {
 	var port string = os.Getenv("APP_SERVER_PORT")
