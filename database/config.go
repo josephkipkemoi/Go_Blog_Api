a9104c888f9203b493c6cceae37c59410ed8f310
jkemboe <jkemboe@gmail.com>
2024-03-07 23:10:16 +0300

    DB env Variables

--- database/config.go
+++ database/config.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,16 +11,14 @@ import (
 
 var DB *gorm.DB
 
-const (
-	host     = "dpg-cnhmv9779t8c73er5n9g-a"
-	port     = "5432"
-	user     = "f1secretsdb_user"
-	password = "o9oLdKmDUsE8jfqR2xq8mnOzWEfJjkFW"
-	dbname   = "f1secretsdb"
-)
-
 // ConnectServer is responsible for setting up and launching the database
 func ConnectDatabase() {
+	var host string = os.Getenv("DB_HOST")
+	var port string = os.Getenv("DB_PORT")
+	var user string = os.Getenv("DB_USER")
+	var password string = os.Getenv("DB_PASSWORD")
+	var dbname string = os.Getenv("DB_NAME")
+
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s	password=%s	dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	Db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
