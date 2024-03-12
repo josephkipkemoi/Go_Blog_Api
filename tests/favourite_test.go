a5a6334142f6c4da105c7ce700fa7cb9c69aae5a
jkemboe <jkemboe@gmail.com>
2024-03-12 09:42:44 +0300

    Favorites test

---
+++ tests/favourite_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"f1-blog/database"
+	"f1-blog/server"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-playground/assert"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println("error loading .env file: ", err)
+	}
+}
+
+func init() {
+	database.ConnectDatabase()
+}
+
+func TestClientCanPostFavourites(t *testing.T) {
+	s := server.ConnectServer()
+
+	fav := &database.Favourite{
+		BlogId: 1,
+		UserId: 2,
+	}
+
+	d, err := json.Marshal(fav)
+	checkErr(err)
+
+	body := bytes.NewReader(d)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/favourites", body)
+	s.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+}
